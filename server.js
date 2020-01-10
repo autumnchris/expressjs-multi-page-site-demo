4ee67938007294ca6c3dcacc0a12f8fdd2c46404
autumnchris <autumncbullard@gmail.com>
2020-01-09 19:21:47 -0500

    Set up EJS as the view engine

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
+
 app.listen(port, console.log(`Server is listening at port ${port}.`));
 
 module.exports = app;
