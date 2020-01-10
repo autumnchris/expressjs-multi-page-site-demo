abdb7f252c93d5ec6d55881422712a70ae84ce8c
autumnchris <autumncbullard@gmail.com>
2020-01-10 00:50:49 -0500

    Use Express routing and EJS template engine to route to all pages

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.render('index', {page: 'Home'});
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('index', {page: 'About'});
+});
+
+router.get('/contact-me', (req, res, next) => {
+  res.render('index', {page: 'Contact Me'});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const routes = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
+
+app.use('/', routes);
 
 app.listen(port, console.log(`Server is listening at port ${port}.`));
 
