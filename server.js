bada3da60d75270bf5bfcd881df8ac2cfc06b611
autumnchris <autumncbullard@gmail.com>
2020-01-13 10:48:25 -0500

    Update syntax

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/', indexRouter);
 
 app.use((req, res, next) => {
-    res.status(404).render('404.ejs', {page: 'Page not found'});
+    res.status(404).render('404', {page: 'Page not found'});
 });
 
 app.listen(port, console.log(`Server is listening at port ${port}.`));
