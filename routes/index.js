316be86bce7913d10cd604678bcdf9efb910f0b4
autumnchris <autumncbullard@gmail.com>
2022-03-13 13:07:51 -0400

    Reorganize app content

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', {page: 'Home'});
+  res.render('index', { page: 'Home' });
 });
 
 router.get('/about', (req, res, next) => {
-  res.render('index', {page: 'About'});
+  res.render('index', { page: 'About' });
 });
 
 router.get('/contact-me', (req, res, next) => {
-  res.render('index', {page: 'Contact Me'});
+  res.render('index', { page: 'Contact Me' });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(expressLayouts);
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
@@ -12,7 +14,7 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/', indexRouter);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {page: 'Page not found'});
+    res.status(404).render('404', { page: 'Page not found' });
 });
 
 app.listen(port, console.log(`Server is listening at port ${port}.`));
